f2dadd2ad099c63d56b376b8208953ee5230c125
SAIKIRANSURAPALLI <surapallisaikiran@gmail.com>
2024-09-07 01:33:53 +0530

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -37,7 +37,7 @@ const admin = () => {
     LOAD_DATA();
   }, [address]);
   return (
-    <>
+    <div classname="body-backgroundColor">
       <Header page={"admin"} />
       <AdminHead />
       <Admin
@@ -58,7 +58,7 @@ const admin = () => {
       <ICOSale setLoader={setLoader} />
       {checkAdmin && <Auth />}
       {loader && <Loader />}
-    </>
+    </div>
   );
 };
 
